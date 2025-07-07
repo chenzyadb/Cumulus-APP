c7ca079a6be6ef8e4e5d2e6fda179749495e2bd6
chenzyadb <89340470+chenzyadb@users.noreply.github.com>
2025-07-07 16:44:48 +0800

    Remove unused dependencies and clean up build files
    Removed unused libraries and test dependencies from app/build.gradle.kts and gradle/libs.versions.toml. Deleted obsolete .gitignore files, Kotlin session, and release output/baseline profile artifacts. Updated .gitignore to exclude release and .kotlin directories for better project hygiene.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,11 @@ plugins {
     alias(libs.plugins.compose.compiler)
 }
 
+// noinspection GradleDependency
 android {
     compileSdk = 34
+
+    // noinspection OldTargetApi
     defaultConfig {
         applicationId = "cumulus.battery.stats"
         minSdk = 28
@@ -13,7 +16,6 @@ android {
         versionCode = 100050
         versionName = "1.0.5"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -58,24 +60,12 @@ android {
 }
 
 dependencies {
-    implementation(libs.compose.markdown)
-    implementation(libs.coil)
     implementation(libs.appcompat)
     implementation(libs.core.ktx)
     implementation(libs.compose.ui)
     implementation(libs.compose.material3)
     implementation(libs.compose.material)
-    implementation(libs.ui.tooling.preview)
     implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.activity.compose)
     implementation(libs.material)
-
-    testImplementation(libs.junit.junit11)
-
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(libs.ui.test.junit4)
-
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
 }
